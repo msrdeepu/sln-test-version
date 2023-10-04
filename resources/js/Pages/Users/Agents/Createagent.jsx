bac03daa703d98d3386f01ced22fb6cba7a4f319
msrdeepu <mspreddy2021@gmail.com>
2023-10-04 16:45:38 +0530

    agents-form-footer

--- resources/js/Pages/Users/Agents/Createagent.jsx
+++ resources/js/Pages/Users/Agents/Createagent.jsx
@@ -1,7 +1,11 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
-import { Card, Typography, Tabs, Form } from "antd";
+import { Head, router } from "@inertiajs/react";
+import { Card, Typography, Tabs, Form, Button } from "antd";
 
+const onCancelData = () => {
+    window.alert("Are You Sure Want to Cancel?");
+    router.get(route("agents.index"));
+};
 
 //tabs
 import Bankdetails from "./FormTabs/Bankdetails";
@@ -15,41 +19,70 @@ const onChange = (key) => {
 };
 const items = [
     {
-        key: '1',
-        label: 'General Details',
-        children: (<General />),
+        key: "1",
+        label: "General Details",
+        children: <General />,
     },
     {
-        key: '2',
-        label: ' Chain & Education',
-        children: (<Education />),
+        key: "2",
+        label: " Chain & Education",
+        children: <Education />,
     },
     {
-        key: '3',
-        label: 'Location',
-        children: (<Location />),
+        key: "3",
+        label: "Location",
+        children: <Location />,
     },
     {
-        key: '4',
-        label: 'Joining & Bank Details',
-        children: (<Bankdetails />),
+        key: "4",
+        label: "Joining & Bank Details",
+        children: <Bankdetails />,
     },
 
     {
-        key: '5',
-        label: 'Nominee',
-        children: (<Nominee />),
+        key: "5",
+        label: "Nominee",
+        children: <Nominee />,
     },
 ];
 
-function Createagent(props) {
+function Createagent({ props, saveButton }) {
     return (
         <>
             <Head title="Agents" />
 
             <Card title={`Create Agent`}>
                 <Form layout="vertical">
-                    <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+                    <Tabs
+                        defaultActiveKey="1"
+                        items={items}
+                        onChange={onChange}
+                    />
+                    <div style={{ textAlign: "center" }}>
+                        <p>
+                            Note: Fields Marked with (
+                            <span style={{ color: "red" }}>*</span>) Astrik is
+                            Mandatory.
+                        </p>
+                    </div>
+                    <div className="btns-container">
+                        <Button
+                            //onClick={handelForm}
+                            htmlType="submit"
+                            className="btn-item"
+                            type="primary"
+                        >
+                            {(saveButton = "Save")}
+                        </Button>
+                        <Button
+                            onClick={onCancelData}
+                            className="btn-item"
+                            type="primary"
+                            danger
+                        >
+                            Cancel
+                        </Button>
+                    </div>
                 </Form>
             </Card>
         </>
